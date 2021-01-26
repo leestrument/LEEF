5bda9476f4212aacdc8533792c263cc932370884
이혁기 <leestrument@hanmail.net>
2021-01-27 00:09:40 +0900

    Completed SubNote

--- core/subNote.ts
+++ core/subNote.ts
@@ -1,9 +1,15 @@
+export const SUBNOTE_VELOCITY_MIN = 1
+export const SUBNOTE_VELOCITY_MAX = 127
+export const SUBNOTE_VELOCITY_DEF = 64
+export const SUBNOTE_GATE_MIN = 0
+export const SUBNOTE_GATE_MAX = 1
+
 export class SubNote {
 
     constructor(
 
-        private _velocity : number,
-        private _gate : number,
+        private _velocity: number = SUBNOTE_VELOCITY_DEF,
+        private _gate: number = SUBNOTE_GATE_MAX,
 
     ){}
 

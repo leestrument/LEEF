de6ea9bb512745c08f6d3d7231698949ce1c3862
이혁기 <leestrument@hanmail.net>
2021-01-28 21:52:15 +0900

    Implemented HybridNote

--- core/note/hybridNote/hybridNote.ts
+++ core/note/hybridNote/hybridNote.ts
@@ -1,7 +1,10 @@
-import { BaseNote } from '../baseNote/baseNote'
-import { SubNote } from './subNote'
-import { VelocityCurve } from './velocityCurve'
-import LeeArray from '../../../helper/leeArray'
+import { BaseNote }             from '../baseNote/baseNote'
+import { StartRandomizer }      from '../randomizer/startRandomizer'
+import { GateRandomizer }       from '../randomizer/gateRandomizer'
+import { SubNote }              from './subNote'
+import { VelocityCurveType }    from './velocityCurveType'
+import { VelocityCurve }        from './velocityCurve'
+import LeeArray                 from '../../../helper/leeArray'
 
 export const HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN = 1
 export const HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MAX = 32
@@ -14,8 +17,10 @@ export class HyBridNote extends BaseNote {
         start                           : number, 
         duration                        : number,
         private _visibleSubNoteCount    = HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN,
-        private _velocityCurve          = new VelocityCurve,
         private _subNotes               = Array.from({length : HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MAX }, () => new SubNote),
+        private _startRandomizer        = new StartRandomizer,
+        private _velocityCurve          = new VelocityCurve,
+        private _gateRandomizer         = new GateRandomizer,
         
     ){ super(pitch, start, duration) }
 
@@ -34,6 +39,33 @@ export class HyBridNote extends BaseNote {
     public decreaseVisibleSubNoteCount(): void {
 
         if (this._visibleSubNoteCount > HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN) this._visibleSubNoteCount--
+
+    }
+
+    public updateStartByStartRandomizer(): void {
+
+        // do stuff in Max side..
+
+    }
+    public updateSubNotesVelocityByVelocityCurve(): void {
+
+        const curveType = this._velocityCurve.getType()
+        const min       = this._velocityCurve.getMin()
+        const max       = this._velocityCurve.getMax()
+
+        switch (curveType) {
+
+            case VelocityCurveType.Up       : this.setSubNotesVelocityUp(min, max); break;
+            case VelocityCurveType.Down     : this.setSubNotesVelocityDown(min, max); break;;
+            case VelocityCurveType.Random   : this.setSubNotesVelocityRandom(min, max); break;;
+            case VelocityCurveType.Fixed    : this.setSubNotesVelocityFixed(max); break;
+
+        }
+
+    }
+    public updateSubNotesGateByGateRandomizer(): void {
+
+        LeeArray.random(this._visibleSubNoteCount, this._gateRandomizer.getMin(), this._gateRandomizer.getMax()).forEach((randomGate, index) => this._subNotes[index].setGate(randomGate))
 
     }
  

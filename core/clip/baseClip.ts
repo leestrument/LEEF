119ad2bf215ec2985fd4f77e38438468e88144eb
이혁기 <leestrument@hanmail.net>
2021-01-29 04:29:06 +0900

    Implemented basdClip

--- core/clip/baseClip.ts
+++ core/clip/baseClip.ts
@@ -1,5 +1,41 @@
+export const BASE_CLIP_IS_SELECTED_DEF = false
+
 export class BaseClip {
 
-    constructor(){}
+    constructor(
+
+        private _color      = 'black',
+        private _isSelected = BASE_CLIP_IS_SELECTED_DEF
+
+    ){}
+
+    // setter
+    public setColor(color: string): void {
+
+        this._color = color
+
+    }
+    public select(): void {
+
+        this._isSelected = true
+
+    }
+    public deselect(): void {
+
+        this._isSelected = false
+
+    }
+
+    // getter
+    public getColor(): string {
+
+        return this._color
+
+    }
+    public isSelected(): boolean {
+
+        return this._isSelected
+
+    }
 
 }

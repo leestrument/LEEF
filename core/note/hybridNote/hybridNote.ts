11876b8763b64896d17b8c6761808f42ffbed4f7
이혁기 <leestrument@hanmail.net>
2021-01-28 20:18:42 +0900

    Implemented setter and getter for visibleSubNoteCount

--- core/note/hybridNote/hybridNote.ts
+++ core/note/hybridNote/hybridNote.ts
@@ -11,11 +11,37 @@ export class HyBridNote extends BaseNote {
         pitch                           : number, 
         start                           : number, 
         duration                        : number,
+        private _visibleSubNoteCount    = HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN,
         private _subNotes               = Array.from({length : HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MAX }, () => new SubNote),
-        private _visibleSubNoteCount    = HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN
         
     ){ super(pitch, start, duration) }
 
+
+    // setter
+    public setVisibleSubNoteCount(subNoteCount: number): void {
+
+        this._visibleSubNoteCount = subNoteCount
+
+    }
+    public increaseVisibleSubNoteCount(): void {
+
+        if (this._visibleSubNoteCount < HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MAX) this._visibleSubNoteCount++
+        
+    }
+    public decreaseVisibleSubNoteCount(): void {
+
+        if (this._visibleSubNoteCount > HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN) this._visibleSubNoteCount--
+
+    }
+
+    // getter
+    public getVisibleSubNoteCount(): number {
+
+        return this._visibleSubNoteCount
+
+    }
+
+    // implement abstract methods of BaseNote
     public updateStartByRandomizer(): void {}
     public updateVelocityByRandomizer(): void {}
     public updateGateByRandomizer(): void {}

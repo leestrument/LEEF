642f03870500701ab1bc1cf9ed23c39970bb3e58
이혁기 <leestrument@hanmail.net>
2021-01-28 20:12:33 +0900

    Renamed constant

--- core/note/hybridNote/hybridNote.ts
+++ core/note/hybridNote/hybridNote.ts
@@ -1,8 +1,8 @@
 import { BaseNote } from '../baseNote/baseNote'
 import { SubNote }  from './subNote'
 
-export const HYBRID_NOTE_SUB_NOTES_COUNT_MIN = 1
-export const HYBRID_NOTE_SUB_NOTES_COUNT_MAX = 32
+export const HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN = 1
+export const HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MAX = 32
 
 export class HyBridNote extends BaseNote {
 
@@ -11,8 +11,8 @@ export class HyBridNote extends BaseNote {
         pitch                           : number, 
         start                           : number, 
         duration                        : number,
-        private _subNotes               = Array.from({length : HYBRID_NOTE_SUB_NOTES_COUNT_MAX}, () => new SubNote),
-        private _visibleSubNoteCount    = HYBRID_NOTE_SUB_NOTES_COUNT_MIN
+        private _subNotes               = Array.from({length : HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MAX }, () => new SubNote),
+        private _visibleSubNoteCount    = HYBRID_NOTE_VISIBLE_SUB_NOTES_COUNT_MIN
         
     ){ super(pitch, start, duration) }
 

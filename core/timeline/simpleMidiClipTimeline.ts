6bb6625f1ef7ad2bfa678096f783444e00bfaeae
이혁기 <leestrument@hanmail.net>
2021-01-29 19:08:01 +0900

    Renamed constant

--- core/timeline/simpleMidiClipTimeline.ts
+++ core/timeline/simpleMidiClipTimeline.ts
@@ -1,12 +1,12 @@
 import { MidiClip } from '../clip/midiClip'
 
-const CLIP_COUNT_DEF = 8
+const MIDI_CLIP_COUNT_DEF = 8
 
 export class SimpleMidiClipTimeline {
 
     constructor(
 
-        private _midiClips = Array.from({ length: CLIP_COUNT_DEF }, () => new MidiClip) 
+        private _midiClips = Array.from({ length: MIDI_CLIP_COUNT_DEF }, () => new MidiClip) 
 
     ){}
 
